512d841e6baa8b63681e6746152ffc0d1cd4cc1a
hack2hr <hack2.hr2@gmail.com>
2020-11-13 18:02:52 +0000

    13.11.2020 server commit

--- Server/kadryapp/views.py
+++ Server/kadryapp/views.py
@@ -16,13 +16,28 @@ def index(request):
     response = JsonResponse(json_util.dumps({"test":123 }), safe = False)
     response["Access-Control-Allow-Origin"] = "*"
     return response
-
+ 
 @csrf_exempt
-def postRequest(request):
-    print(request.POST)
-    title = request.POST.get("title")
-    response = JsonResponse(json_util.dumps({"test":title }), safe = False)
+def testPost(request):
+    if(request.method == "OPTIONS"): 
+        response = JsonResponse({})
+        response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+        response["Access-Control-Allow-Origin"] = "http://13.79.21.196:8080"
+        response["Access-Control-Allow-Headers"] = "origin, x-requested-with, content-type"
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    #content = body['content']
+    print(body)
+    response = JsonResponse(json_util.dumps({"test: 12"}), safe = False)
     response["Access-Control-Allow-Origin"] = "*"
     return response
 
+@csrf_exempt 
+def testGet(request):
+    testVal = request.GET.get("testVal")
+    response = JsonResponse(json_util.dumps({"test": testVal}), safe = False)
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
 
